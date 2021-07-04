a561336867b866dc38bd8347ba23962d8b19d7ea
Shreedhar <meshree4@gmail.com>
2021-07-04 15:05:53 +0530

    CSS changed for homepage, sign-in and sign-up form.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     if session[:current_user_id]
       @user = User.find_by(id: session[:current_user_id])
+      @owner = Owner.find_by(id: session[:current_user_id])
     end
   end
 end
