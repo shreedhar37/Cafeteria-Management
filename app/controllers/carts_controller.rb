ed6f398c90247e1c78f58b9c051efc670ad59675
Shreedhar <meshree4@gmail.com>
2021-07-18 23:59:37 +0530

    User can now delete orders from cart.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,27 @@
 class CartsController < ApplicationController
   def show
-    @cart = Cart.where(:user_id => params[:id])
-    render :index
+    if @user
+      @cart = Cart.where(:user_id => params[:id])
+      render :index
+    else
+      redirect_to "/"
+    end
+  end
+
+  def destroy
+    if @user
+      cart_item = Cart.find(params[:id])
+      if cart_item.present?
+        flash[:notice] = cart_item.submenu_items_description + " removed."
+        cart_item.destroy
+        redirect_to "/carts/" + session[:current_user_id].to_s
+      else
+        flash[:alert] = "Sorry, unable to find the your order."
+        redirect_to "/carts/" + session[:current_user_id].to_s
+      end
+    else
+      redirect_to "/"
+    end
   end
 
   def create
@@ -10,7 +30,7 @@ class CartsController < ApplicationController
       parameters[:submenu_item_price] = parameters[:submenu_item_price].to_f * parameters[:quantity].to_f
       @cart = Cart.new(parameters)
       if @cart.save
-        flash[:notice] = params[:submenu_items_description] + " successfully added to cart."
+        flash[:notice] = params[:quantity].to_s + " " + params[:submenu_items_description] + " added to cart."
         redirect_to submenu_items_path
       else
         flash[:alert] = "We are sorry, something went wrong."
